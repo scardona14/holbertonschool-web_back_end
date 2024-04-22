2091eb05bef61117a1400a684a57334201d2a605
scardona14 <cardanasean123@gmail.com>
2024-04-22 13:36:25 -0400

    Fixed documentation

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -2,6 +2,7 @@
 """Lets duck type an iterable object"""
 from typing import List, Tuple, Sequence, Iterable, Mapping, MutableMapping
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """Element length function"""
     return [(i, len(i)) for i in lst]
