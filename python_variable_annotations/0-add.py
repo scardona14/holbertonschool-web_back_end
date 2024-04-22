9674ef0af81a96e47a1956652bec0d93cd15464a
scardona14 <cardanasean123@gmail.com>
2024-04-22 13:46:07 -0400

    Fixed documentation with pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-"""Type-annotated function add that takes a float a and a float b as arguments and returns their sum as a float."""
+
+"""
+Type-annotated function add that takes a float a and a float b as arguments and
+returns their sum as a float.
+"""
+
+
 def add(a: float, b: float) -> float:
     """Return sum of two floats"""
     return a + b
--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
-"""Type-annotated function floor that takes a float n as argument and returns the floor of the float."""
+"""
+Type-annotated function floor that takes a float n as argument and
+returns the floor of the float.
+"""
 import math
+
+
 def floor(n: float) -> int:
     """Return floor of float"""
     return math.floor(n)
