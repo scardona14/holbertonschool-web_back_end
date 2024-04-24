c50670e705714676cfbf70158385964cda493b7d
scardona14 <cardanasean123@gmail.com>
2024-04-24 11:19:32 -0400

    Async Generator

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Async Generator"""
+
+from asyncio import sleep
+from random import uniform
+from typing import AsyncGenerator, Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Async Generator"""
+    for _ in range(10):
+        await sleep(1)
+        yield uniform(0, 10)
