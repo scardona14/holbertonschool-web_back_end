6b4f626d8c089a02c38805d06f24ca8d82cd2a8e
scardona14 <cardanasean123@gmail.com>
2024-05-07 10:39:02 -0400

    Block scope

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,12 +1,13 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
+  const task = false;
+  const task2 = true;
 
-    if (trueOrFalse) {
-        /* eslint-disable no-console */
-        const task = true;
-        const task2 = false;
-    }
+  if (trueOrFalse) {
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
+  }
 
-    return [task, task2];
+  return [task, task2];
 }
