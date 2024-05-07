1aac69c1d90a412f7e93a703928fbbc945c13cb4
scardona14 <cardanasean123@gmail.com>
2024-05-07 10:13:28 -0400

    Fixed eslint documentation

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
 export function taskFirst() {
   const task = 'I prefer const when I can.';
-    return task;
+  return task;
 }
 
 export function getTask() {
-    return ' is okay';
+  return ' is okay';
 }
 
 export function taskNext() {
-    let combination = 'But sometimes let';
-    combination += getTask();
+  let combination = 'But sometimes let';
+  combination += getTask();
 
-    return combination;
+  return combination;
 }
