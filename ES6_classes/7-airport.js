b1070e7f622d23e7b7d95306ea086a1450337e0e
scardona14 <cardanasean123@gmail.com>
2024-05-09 14:48:25 -0400

    Implementing Airport class

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,12 @@
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get [Symbol.toStringTag]() {
+    return this.code;
+  }
+}
+
+export default Airport;
