c3b3a0dde13e4f007d4174e2dbeec2791c8b1586
scardona14 <cardanasean123@gmail.com>
2024-05-17 14:37:44 -0400

    Fixing eslint errors

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,11 @@
 function handleResponseFromAPI(promise) {
-  return promise.then(() => {
-    try {
-      return {
+    return promise
+      .then(() => ({
         status: 200,
         body: 'success',
-      };
-    } catch (error) {
-      throw new Error('The fake API is not working currently');
-    }
-  })
-    .catch(() => new Error('The fake API is not working currently'))
-    .finally(() => console.log('Got a response from the API'));
-}
-
-export default handleResponseFromAPI;
+      }))
+      .catch(() => Error())
+      .finally(() => console.log('Got a response from the API'));
+  }
+  
+  export default handleResponseFromAPI;
