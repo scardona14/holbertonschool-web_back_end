09c1ca19fe4b9b317dbf8c22abc7feea7797f425
scardona14 <cardanasean123@gmail.com>
2024-05-20 15:40:42 -0400

    Fix typo in the ID

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ function getListStudents() {
   const objects = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 3, firstName: 'Serena', location: 'San Francisco' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
 
   return objects;
