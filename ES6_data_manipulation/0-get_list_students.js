ec64f7ce1c26e9a814a4f92ff8a98a7c678f5d12
scardona14 <cardanasean123@gmail.com>
2024-05-20 15:35:02 -0400

    Adding the array of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,9 +1,11 @@
 function getListStudents() {
-  return [
+  const objects = [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 3, firstName: 'Serena', location: 'San Francisco' },
   ];
+
+  return objects;
 }
 
 export default getListStudents;
