6569c8e6bc94e91c5b01f28e360a9bb723e7ddad
scardona14 <cardanasean123@gmail.com>
2024-05-21 14:54:34 -0400

    Creating a function named updateUniqueItems that returns an updated map for all items with initial quantity at 1.

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,13 @@
+function updateUniqueItems(map) {
+    if (!map instanceof Map) throw Error('Cannot process');
+}
+
+for (const [key, value] of map) {
+    if (value === 1) {
+        map.set(key, 100);
+    }
+
+    return map;
+}
+
+export default updateUniqueItems;
