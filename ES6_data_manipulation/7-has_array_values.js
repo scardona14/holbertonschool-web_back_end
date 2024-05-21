256eaea36f247405f1a70e7fe78c923ba95fd276
scardona14 <cardanasean123@gmail.com>
2024-05-21 12:27:15 -0400

    Addifn parentheses around arrow function

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,6 +1,6 @@
 function hasValuesFromArray(set, array) {
-    const check = array.every(value => set.has(value));
-    return check;
+  const check = array.every((value) => set.has(value));
+  return check;
 }
 
 export default hasValuesFromArray;
