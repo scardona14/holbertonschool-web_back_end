8787dfaae08b8b6b19349a7748f4639c864b30a1
scardona14 <cardanasean123@gmail.com>
2024-05-21 21:10:47 -0400

    Fixed eslint documentation

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,12 @@
 function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  if (position >= length) {
+  const view = new Int8Array(buffer, 0);
+  if (position > length - 1) {
     throw Error('Position outside range');
   }
-  int8View[position] = value;
-  return int8View;
+  view.setInt8(position, value);
+
+  return view;
 }
 
 export default createInt8TypedArray;
