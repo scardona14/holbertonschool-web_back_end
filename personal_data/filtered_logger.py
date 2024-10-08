e87e63ee54bfd8da9a3a3b77f077f4c1faae6615
scardona14 <cardanasean123@gmail.com>
2024-10-08 16:42:55 -0400

    Updating the class to accept a list of strings fields constructor argument

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -52,12 +52,12 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ';'
 
-def __init__(self):
+def __init__(self, fields: List[str]):
     """
     Constructor
     """
     super(RedactingFormatter, self).__init__(self.FORMAT)
-    self.fields = PII_FIELDS
+    self.fields = fields
 
 def format(self, record: logging.LogRecord) -> str:
     """
