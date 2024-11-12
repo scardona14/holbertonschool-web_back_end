b7b26d76a7efc14be80efed8f18d465b81b9cf03
scardona14 <cardanasean123@gmail.com>
2024-11-12 09:53:31 -0400

    Added more comments

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,5 @@
 -- create a table named users
+-- 1. In and not outside the database
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
